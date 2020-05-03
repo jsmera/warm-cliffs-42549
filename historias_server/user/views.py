278cc041534fa28ed2261064b572cd355ae3303f
Carlos Mera <merac1999@gmail.com>
2020-05-02 22:01:30 -0500

    Filtro para usuarios y eliminacion

--- historias_server/user/views.py
+++ historias_server/user/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse, reverse_lazy
 from .models import Usuario
 from .forms import CreateUsuarioForm, UpdateUsuarioForm
+from django.db.models import Q
 
 
 class AdminRequiredMixin(UserPassesTestMixin):
@@ -48,3 +49,31 @@ class DeleteUsuarioView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
 class UsuarioListView(LoginRequiredMixin, AdminRequiredMixin, ListView):
     model = Usuario
     paginate_by = 10
+
+    def get_queryset(self):
+        full_name = self.request.GET.get('full_name', "")
+        username = self.request.GET.get('username', "")
+        email = self.request.GET.get('email', "")
+        rol = self.request.GET.get('rol', "")
+
+        query = Usuario.objects.filter(
+            (Q(first_name__icontains=full_name) | Q(last_name__icontains=full_name)) &
+            Q(username__icontains=username) &
+            Q(email__icontains=email)
+        )
+        if rol != "":
+            query = query.filter(rol=rol)
+        return query
+    
+    def get_context_data(self, **kwargs):
+        base_url = self.request.GET.copy()
+        if "page" in base_url:
+            base_url.pop("page")
+        context = super(UsuarioListView, self).get_context_data(**kwargs)
+        context["full_name"] = self.request.GET.get('full_name', "")
+        context["username"] = self.request.GET.get('username', "")
+        context["email"] = self.request.GET.get('email', "")
+        context["rol"] = self.request.GET.get('rol', "")
+        context["base_url"] = base_url.urlencode
+
+        return context
