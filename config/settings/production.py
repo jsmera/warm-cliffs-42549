4d28437e457eb5d3aa24b4777c1b622cab256ab2
Carlos Mera <merac1999@gmail.com>
2020-05-28 18:08:02 -0500

    Format

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,10 +3,10 @@ from .common import *
 MIDDLEWARE += (
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import django_heroku
