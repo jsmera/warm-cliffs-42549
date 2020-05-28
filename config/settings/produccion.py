950f099d81107b395ed0823654a5cb0328ef9f97
Carlos Mera <merac1999@gmail.com>
2020-05-28 16:28:31 -0500

    Production settings

--- config/settings/produccion.py
+++
@@ -1,9 +0,0 @@
-from .common import *
-
-MIDDLEWARE += (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
